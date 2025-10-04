d2e251fce57146721ab2b114b6f6ef4fc1955fbb
PHOOMPHAT <phoomphat091@gmail.com>
2025-10-04 18:26:43 +0700

    Update uuid.lua

--- uuid.lua
+++ uuid.lua
@@ -1,10 +1,10 @@
 return {
-  enabled = true,
-  ignore_if_missing_uuid = true,
-  
-  ttl_sec = 120,
-  
-  whitelist_userids = {},
-  whitelist_players = {},
-  whitelist_remotes = {}
+    enabled = true,
+    ignore_if_missing_uuid = true,
+    
+    ttl_sec = 120,
+    
+    whitelist_userids = {},
+    whitelist_players = {},
+    whitelist_remotes = {}
 }
