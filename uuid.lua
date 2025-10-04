461e7232e683a6a732d762f9595b6c8d3d248fb8
PHOOMPHAT <phoomphat091@gmail.com>
2025-10-04 18:45:47 +0700

    Update uuid.lua

--- uuid.lua
+++ uuid.lua
@@ -1,11 +1,15 @@
 return {
-    config_rf_name = "Abyss",
-    
     enabled = true,
     ttl_sec = 120,
-    ignore_if_missing_uuid = true,
+    ignore_if_missing_uuid = false,
     
     whitelist_userids = {},
     whitelist_players = {},
     whitelist_remotes = {}
+
+    sec = ttl_sec
+    missuuid = ignore_if_missing_uuid
+    userids = whitelist_userids
+    players = whitelist_players
+    remotes = whitelist_remotes
 }
